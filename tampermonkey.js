12a606f8bac6fa6f8a343c556cd0c36dbb4e5354
Dandan <Dandanmuaa@gmail.com>
2021-10-30 01:22:20 +0800

    Update tampermonkey.js

--- tampermonkey.js
+++ tampermonkey.js
@@ -9,18 +9,13 @@
 // @connect      42.192.47.62
 // @grant        GM_xmlhttpRequest
 // @require      https://lib.baomitu.com/jquery/3.6.0/jquery.min.js
-// @require      https://unpkg.com/sweetalert/dist/sweetalert.min.js
 // @antifeature  ads
 // @antifeature  membership
 // ==/UserScript==
 
 var conf = {
-	title: "MOOC助手",
-	time: 100, //平均时间
-	learn: 1, //自动答题
-	auto: 1, //自动提交
-	random: 0, //无匹配答案随机选择
-    doTimeOut:5000,
+	title: "超星分数查询（油猴中文网首发）",
+    TimeOut:5000,
 };
 
 $('head').append('<link href="https://lib.baomitu.com/layui/2.6.8/css/layui.css" rel="stylesheet" type="text/css" />');
@@ -35,36 +30,40 @@ $.getScript("https://lib.baomitu.com/layui/2.6.8/layui.js", function(data, statu
         console.log(courseid,clazzid)
         layer.closeAll();
         show();
+        $(".start").click(function() {
+            chafen(courseid,clazzid);
+        })
     };
 });
 
-function chafen(courseid,clazzid){
+var chafen = (courseid,clazzid)=>{
     GM_xmlhttpRequest({
         method: 'GET',
         url: "http://42.192.47.62:8080/cha?courseId="+courseid+"&classId="+clazzid,
-        timeout: conf.doTimeOut,
+        timeout: conf.TimeOut,
         onload: function(xhr) {
             if (xhr.status == 200) {
+                var el="";
                 var obj = JSON.parse(xhr.responseText) || {};
-                document.getElementsByClassName("layui-card-header")[0].innerHTML = "油猴中文网首发";
+                document.querySelector("#msg > div").innerHTML = '共获取'+obj.total+'位同学分数';
                 var content = document.getElementsByClassName("layui-card-body")[0]
-                if (obj.score!=[]) {
-                    var count = obj.count.length;
-                    console.log(obj.score.length);
-                    for(let i=0;i<obj.score.length;i=i+(count-4)/2+2){
-                        for(let k=0;k<=(count-4)/2+1;k++){
-                            content.innerHTML += obj.score[k+i] + "　";
+                if (obj.body!=[]) {
+                    el = '<tr class="layui-bg">'
+                    for(let i=0;i<obj.head.length;i++){
+                        el = el + '<td>' + obj.head[i] + '</td>'
+                    }
+                    el = el + '</td></tr>'
+                    $("#content>table>tbody").append($(el));
+                    for(let i=0;i<obj.total;i=i+1){
+                        el = '<tr class="layui-bg">'
+                        for(let k=0;k<obj.head.length;k++){
+                            el = el + '<td>' + obj.body[i][k] + '</td>'
                         }
-                        content.innerHTML += "<br>";
+                        el = el + '</td></tr>'
+                        $("#content>table>tbody").append($(el));
                     }
                 }
-                setTimeout(function(){
-                    swal({
-                        title: "油猴中文网首发",
-                        text: "油猴中文网首发",
-                        icon: "success",
-                        button: "收到",
-                    });},1000);
+                window,open("https://bbs.tampermonkey.net.cn/");
             }
         }
     });
@@ -72,16 +71,16 @@ function chafen(courseid,clazzid){
 var show = ()=>{
 	layer.open({
 		type: 1,
-		area: ['500px', '300px'],
-		offset: 'rb',
+		area: ['800px', '500px'],
+		offset: 'rt',
 		id: 'msgt',
-		closeBtn: 0,
+		closeBtn: 1,
 		title: conf.title,
 		shade: 0,
 		maxmin: true,
 		anim: 2,
-		content: '<div id="msg" ><blockquote class="layui-elem-quote layui-quote-nm"><button type="button"  class="layui-btn layui-btn-normal start">点我获取题库-请确保在试卷页面点击<button></blockquote>'+
-        '<div class="layui-collapse"><div class="layui-colla-item"><h2 class="layui-colla-title">公告</h2><div class="layui-colla-content layui-show"><div>扫描二维码下载APP搜题,题库更全哦~<img src="https://www.chitus.com/ewm/create?config=%7B%22content%22%3A%22https%3A%2F%2Fwww.fenbi.com%2Fdepot%2Ffenbi-search-question%2Findex.html%3Fapp%3Dsouti%26signVendor%3D8DFD180AC8706F9A519739C0F9F42CA6%22%7D" width="100px" /><br>&nbsp;&nbsp;反馈/建议+群:<a href="https://qm.qq.com/cgi-bin/qm/qr?k=kYJMBiYDBazG97Z_L3Nq9ppEexQtXh00&jump_from=webapi=">740894629</a></div></div></div>'
-        +'<div id="content"><ul></ul>		<table class="layui-table"> <colgroup> <col width="50"> <col> <col> </colgroup> <thead> <tr> <th>序号</th> <th>题目</th> <th>答案</th> </tr> </thead> <tbody>  </tbody> </table></div></div></div>'
+		content: '<div id="msg" ><div class="layui-elem-quote" style="margin-left:10px;"><button type="button"  class="layui-btn layui-btn-normal start">点我获取全班成绩<button></div>'+
+        '<div class="layui-collapse"><div class="layui-colla-item"><h2 class="layui-colla-title">公告</h2><div class="layui-colla-content layui-show"><div>每位用户只可以凭token查询一次分数<br>插件和谐不再更新</a></div></div></div>'
+        +'<div id="content"><ul></ul>		<table class="layui-table"> <colgroup> <col width="100"> <col> <col> </colgroup> <thead> <tr>  </tr> </thead> <tbody>  </tbody> </table></div></div></div>'
 	});
 }
